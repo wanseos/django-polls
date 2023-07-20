839ee3b34172503c3c679a9ca420f3bf8be1584e
wan-seo <wseokim@gmail.com>
2023-07-21 03:33:42 +0900

    new: initialize django app (nilla), create model (nilla.User)

--- vanilla/vanilla/settings.py
+++ vanilla/vanilla/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "nilla",
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "nilla.User"
