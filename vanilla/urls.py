ddf7718e29c85b50bcdef3387f854520bbded102
Wanseo <wanseoinbox@gmail.com>
2024-07-16 15:02:28 +0900

    feat: implement example views and admin

--- vanilla/urls.py
+++ vanilla/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("vanilla.nilla.polls.urls")),
+    path("polls/", include("vanilla.nilla.services.polls.urls")),
     path("admin/", admin.site.urls),
 ]
