9a25d6e01c85130c0b7c2eba922f18afe160f029
Wanseo <wanseoinbox@gmail.com>
2024-07-17 11:15:30 +0900

    feat: add example views and tests

--- vanilla/nilla/admin.py
+++ vanilla/nilla/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from .models import polls_models
 
 admin.site.register(polls_models.Question)
+admin.site.register(polls_models.QuestionChoice)
--- vanilla/nilla/services/polls/views.py
+++ vanilla/nilla/services/polls/views.py
@@ -1,27 +1,59 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-from .models import Question
-
-
-def index(request) -> HttpResponse:
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+from .models import Question, QuestionChoice
 
 
-def detail(request, question_id: int) -> HttpResponse:
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id: int) -> HttpResponse:
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id: int) -> HttpResponse:
-    return HttpResponse("You're voting on question %s." % question_id)
+        selected_choice = question.questionchoice_set.get(
+            pk=request.POST["choice"]
+        )  # noqa
+    except (KeyError, QuestionChoice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+            reverse(
+                "polls:results",
+                args=(question.id,),
+            )
+        )
