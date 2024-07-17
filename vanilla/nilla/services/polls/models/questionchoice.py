9b3e3154eab0aa2ab8402ee2b51be29936b41acf
Wanseo <wanseoinbox@gmail.com>
2024-07-17 10:53:11 +0900

    feat: add field `QuestionChoice.votes`

--- vanilla/nilla/services/polls/models/questionchoice.py
+++ vanilla/nilla/services/polls/models/questionchoice.py
@@ -9,6 +9,7 @@ class QuestionChoice(models.Model):
         on_delete=models.CASCADE,
     )
     choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
     class Meta:
         db_table = "polls_questionchoice"
